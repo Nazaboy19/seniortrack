43704ff5e970e872942a561756fa43a0d0d8233a
Ibrahim Mercado <ahmedelgrande1@gmail.com>
2017-03-30 23:59:06 -0400

    beginning of work for seniortrack ios

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { AppRegistry, Image } from 'react-native';
 
-export default class SenoirTrack extends Component {
+class Bananas extends Component {
   render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to Senior Track!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <Image source={pic} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: 'blue',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('SenoirTrack', () => SenoirTrack);
+AppRegistry.registerComponent('Bananas', () => Bananas);
